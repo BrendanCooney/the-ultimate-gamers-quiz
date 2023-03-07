60995a8370290cb53458c25de66837d33a67dea7
BrendanCooney <advicast@gmail.com>
2023-03-07 07:53:12 +0000

    Added functions and questions changed syntax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,64 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
 console.log('Started')
 startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() -.5)
+currentQuestionIndex = 0
 questionContainerElement.classList.remove('hide')
+setNextQuestion()
 }
 
 function setNextQuestion () {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 function selectAnswer() {
     
 }
+
+const questions = [
+    {
+        question: 'What is 2 + 2?',
+        answers: [
+          { text: '4', correct: true },
+          { text: '22', correct: false }
+        ]
+      },
+    {
+      question: 'Who is the best YouTuber?',
+      answers: [
+        { text: 'Web Dev Simplified', correct: true },
+        { text: 'Traversy Media', correct: true },
+        { text: 'Dev Ed', correct: true },
+        { text: 'Fun Fun Function', correct: true }
+      ]
+    },
+    {
+      question: 'Is web development fun?',
+      answers: [
+        { text: 'Kinda', correct: false },
+        { text: 'YES!!!', correct: true },
+        { text: 'Um no', correct: false },
+        { text: 'IDK', correct: false }
+      ]
+    },
+    {
+      question: 'What is 4 * 2?',
+      answers: [
+        { text: '6', correct: false },
+        { text: '8', correct: true }
+      ]
+    }
+  ]
