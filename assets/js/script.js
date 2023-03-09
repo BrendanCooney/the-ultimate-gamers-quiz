86b3c0a49e1c1ba4bba89abd01525cce41250de4
BrendanCooney <advicast@gmail.com>
2023-03-09 08:35:10 +0000

    Adding question changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,38 +76,46 @@ function clearStatusClass(element) {
   element.classList.remove("wrong");
 }
 
-
 const questions = [
-    {
-        question: 'What is 2 + 2?',
-        answers: [
-          { text: '4', correct: true },
-          { text: '22', correct: false }
-        ]
-      },
-    {
-      question: 'Who is the best YouTuber?',
-      answers: [
-        { text: 'Web Dev Simplified', correct: true },
-        { text: 'Traversy Media', correct: true },
-        { text: 'Dev Ed', correct: true },
-        { text: 'Fun Fun Function', correct: true }
-      ]
-    },
-    {
-      question: 'Is web development fun?',
-      answers: [
-        { text: 'Kinda', correct: false },
-        { text: 'YES!!!', correct: true },
-        { text: 'Um no', correct: false },
-        { text: 'IDK', correct: false }
-      ]
-    },
-    {
-      question: 'What is 4 * 2?',
-      answers: [
-        { text: '6', correct: false },
-        { text: '8', correct: true }
-      ]
-    }
-  ]
+  {
+    question: "What is 2 + 2?",
+    answers: [
+      { text: "4", correct: true },
+      { text: "22", correct: false }
+    ]
+  },
+  {
+    question: "Who is the best YouTuber?",
+    answers: [
+      { text: "Web Dev Simplified", correct: true },
+      { text: "Traversy Media", correct: true },
+      { text: "Dev Ed", correct: true },
+      { text: "Fun Fun Function", correct: true }
+    ]
+  },
+  {
+    question: "When was Roblox Started?",
+    answers: [
+      { text: "2006", correct: true },
+      { text: "2007", correct: false },
+      { text: "2008", correct: false },
+      { text: "2004", correct: false }
+    ]
+  },
+  {
+    question: "Is web development fun?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false }
+    ]
+  },
+  {
+    question: "What is 4 * 2?",
+    answers: [
+      { text: "6", correct: false },
+      { text: "8", correct: true }
+    ]
+  }
+];
